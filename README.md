a338b88162a8bdc172735c19cdf7ba9b82ae9aaf
DrakeSai <franartigaf15@gmail.com>
2019-03-31 18:08:25 -0600

    proyect to be evaluated

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # FocusServices
+
 Automation test
+-- on terminal run the following steps
+
+git clone git@github.com:DrakeSai/FocusServices.git
+cd FocusServices/
+npm install
+
+-- on your browser
+Download ChromeDriver for win32 from the following link https://chromedriver.storage.googleapis.com/index.html?path=73.0.3683.68/
+
+-- on your pc
+Unzip the file
+Copy chromedriver.exe
+Paste chromedriver.exe on project folder “FocusServices”
+
+--On terminal type: 
+node Test.js
+
+This projects has been implemented with the version 73.0.3683.68 of chromedriver and google chrome version 73.0.3683.86
--- Test.js
+++ Test.js
@@ -6,16 +6,15 @@ const {Builder, By, Key} = require('selenium-webdriver');
     let driver = await new Builder().forBrowser('chrome').build();
 
     try {
-    // open chrome browser
+
+    //open browser
     await driver.get('http://www.google.com');
-
-    //Search "Focus Services"
+    //find Focus Services
     await driver.findElement(By.name('q')).sendKeys('Focus Services', Key.RETURN);
-
-    // click on Focus Services – Beyond Expectations  hyperlink
+    //go to focus services
     await driver.findElement(By.className('LC20lb')).click();
 
-    // verify if button 'now hiring' exist
+      // Verify if button Now Hiring exist
     var buttonExist = await driver.findElement(By.xpath('//*[@id="text-4"]/div/p/a/span')).then(function() {
         return true;
     }, function(err) {
@@ -23,28 +22,33 @@ const {Builder, By, Key} = require('selenium-webdriver');
             console.log(err);
             return false;
             
-        }
+        } 
     });
   
-    //if button exist keep navigating in FocusServices
     if (buttonExist === true) {
-        //click on tab carrers
+
+        //click tab carrears 
         await driver.findElement(By.xpath('//*[@id="menu-item-6497"]/a/span[2]')).click();
         //filter by inbound
         await driver.findElement(By.xpath('//*[@id="search_keywords"]')).sendKeys('inbound', Key.RETURN);
-        // going to second result 
+        // click on second element
         await driver.findElement(By.xpath('//*[@id="317531"]/div[1]/h3')).click();
-        // verify that job description exist
-        await driver.findElement(By.xpath('//*[@id="description"]/p[3]/span')).then(console.log('Element "jobs description" exists'));
+        //verify job description
+        await driver.findElement(By.xpath('//*[@id="description"]/p[3]/span')).then(console.log('job description exists '));
         
     }
+   
+
 
     }
+
     catch (e) {
         console.log(e);
     }
 
     finally {
+
+    // await driver.quit();
 
     }
 
